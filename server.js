c2ea141fb17dfdc36b28a5c7754cec765946c582
Ryan Esmores <ryanesmores07@gmail.com>
2023-12-27 00:20:10 +0900

    Get Single Job method done

--- server.js
+++ server.js
@@ -51,6 +51,25 @@ app.post("/api/v1/jobs", (req, res) => {
   });
 });
 
+// GET SINGLE JOB
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({
+      status: "error",
+      message: `There is no job with the id of ${id}`,
+    });
+  }
+
+  status: "error",
+    res.status(200).json({
+      status: "success",
+      job,
+    });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
