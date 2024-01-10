dc5926e9167c5a32a9496e6e1326c86c101c6ad6
Ryan Esmores <eesmores@fab.pasona.tech>
2024-01-10 17:28:51 +0900

    Continue with Update user

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,3 +44,12 @@ export const login = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "user logged in" });
   // Check if password is correct
 };
+
+export const logout = (req, res) => {
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+
+  res.status(StatusCodes.OK).json({ msg: "user logged out!" });
+};
