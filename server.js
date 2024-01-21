a93fe6874c1399d611150959088f450d4f8668c7
Ryan Esmores <ryanesmores07@gmail.com>
2024-01-22 00:32:12 +0900

    Continue with Multer

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
 import userRouter from "./routes/userRouter.js";
 
+// public
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 // middlewares
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
@@ -18,10 +22,11 @@ import { authenticateUser } from "./middleware/authMiddleware.js";
 const app = express();
 const port = process.env.PORT || 5100;
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
+app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(cookieParser());
 app.use(express.json());
 
