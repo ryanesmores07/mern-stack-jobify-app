3b841b950aba0a3709851fc8fe2f602cd2eb3367
Ryan Esmores <eesmores@fab.pasona.tech>
2024-01-25 13:06:01 +0900

    Done with Search Form Setup and Controlled Inputs. Continue with Debounce

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -4,9 +4,54 @@ import mongoose from "mongoose";
 import day from "dayjs";
 
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId });
+  const { search, jobStatus, jobType, sort } = req.query;
+
+  const queryObject = {
+    createdBy: req.user.userId,
+  };
+
+  if (search) {
+    queryObject.$or = [
+      { position: { $regex: search, $options: "i" } },
+      { company: { $regex: search, $options: "i" } },
+    ];
+  }
+
+  if (jobStatus && jobStatus !== "all") {
+    queryObject.jobStatus = jobStatus;
+  }
+  if (jobType && jobType !== "all") {
+    queryObject.jobType = jobType;
+  }
+
+  const sortOptions = {
+    newest: "-createdAt",
+    oldest: "createdAt",
+    "a-z": "position",
+    "z-a": "-position",
+  };
+
+  const sortKey = sortOptions[sort] || sortOptions.newest;
+
+  // Setup pagination
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const jobs = await Job.find(queryObject)
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit);
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
   res.status(StatusCodes.OK).json({
     status: "success",
+    totalJobs,
+    numOfPages,
+    currentPage: page,
     jobs,
   });
 };
@@ -106,6 +151,5 @@ export const showStats = async (req, res) => {
     })
     .reverse();
 
-  
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
